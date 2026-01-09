c5b159f79c23a989b458f3175a122fa739102742
Swellington Soares <qb.corebr@gmail.com>
2026-01-08 22:12:02 -0300

    Configuração inicial do projeto da aula de kafka

---
+++ pixprodutor/src/main/java/com/alura/pix/service/PixService.java
@@ -0,0 +1,26 @@
+package com.alura.pix.service;
+
+import com.alura.pix.dto.PixDTO;
+import com.alura.pix.model.Pix;
+import com.alura.pix.repository.PixRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PixService {
+
+    
+    private final PixRepository pixRepository;
+
+    
+    private final KafkaTemplate<String, PixDTO>  kafkaTemplate;
+
+    public PixDTO salvarPix(PixDTO pixDTO) {
+        pixRepository.save(Pix.toEntity(pixDTO));
+        kafkaTemplate.send("pix-topic", pixDTO.getIdentifier(), pixDTO);
+        return pixDTO;
+    }
+
+}
