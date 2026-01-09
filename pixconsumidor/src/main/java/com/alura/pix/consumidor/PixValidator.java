8a071ec8561579b0a51bf04e9d43b6766cf931c9
Swellington Soares <qb.corebr@gmail.com>
2026-01-09 00:24:02 -0300

    adição do módulo pixstream

--- pixconsumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
+++ pixconsumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
@@ -2,12 +2,15 @@ package com.alura.pix.consumidor;
 
 import com.alura.pix.dto.PixDTO;
 import com.alura.pix.dto.PixStatus;
+import com.alura.pix.exception.KeyNotFoundException;
 import com.alura.pix.model.Key;
 import com.alura.pix.model.Pix;
 import com.alura.pix.repository.KeyRepository;
 import com.alura.pix.repository.PixRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +22,12 @@ public class PixValidator {
     private final PixRepository pixRepository;
 
     @KafkaListener(topics = "pix-topic", groupId = "grupo")
+    @RetryableTopic(
+            backoff = @Backoff(value = 3000L),
+            attempts = "5",
+            autoCreateTopics = "true",
+            include = KeyNotFoundException.class
+    )
     public void processaPix(PixDTO pixDTO) {
         System.out.println("Pix  recebido: " + pixDTO.getIdentifier());
 
@@ -29,6 +38,7 @@ public class PixValidator {
 
         if (origem == null || destino == null) {
             pix.setStatus(PixStatus.ERRO);
+            throw new KeyNotFoundException();
         } else {
             pix.setStatus(PixStatus.PROCESSADO);
         }
